88ec58bbf4a6eff40c2d837ce9e9d55d80c6403c
Alexey <alexey@norlin.ru>
2014-12-23 17:57:39 +0300

    update error page

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (false || app.get('env') === 'development') {
+if (false) {
 	app.use(function(err, req, res, next) {
 		res.status(err.status || 500);
 		res.render('error', {
