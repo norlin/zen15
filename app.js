d20b5f031f6f25b22d8eb3e0fbe513ee651b8b6f
Alexey <alexey@norlin.ru>
2014-12-24 14:57:48 +0300

    add long cache headers

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31536000000}));
 
 app.use('/css', expressLess(__dirname + '/less', { compress: true }));
 
