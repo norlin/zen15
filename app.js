0b596c1cb8e8f8e4e382c6fb24528b9f0c7e6558
Alexey <alexey@norlin.ru>
2014-12-29 11:11:12 +0300

    add kitty

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var check_ip = require('./routes/check_ip');
+
 var routes = require('./routes/index');
 var count = require('./routes/count');
+var kitty = require('./routes/kitty');
 
 var dust = require('dustjs-linkedin');
 var cons = require('consolidate');
@@ -21,10 +24,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/kitty', kitty);
+app.use(check_ip);
+
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31536000000}));
 
 app.use('/css', expressLess(__dirname + '/less', { compress: true }));
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,21 @@ var router = express.Router();
 
 var bgs = ['1', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];
 var bgCount = bgs.length-1;
-/* GET home page. */
-router.get('/', function(req, res) {
+
+function home(template, req, res){
 	var num = Math.round(Math.random()*bgCount);
 	var bg = bgs[num];
 	var count = req.app.get('count') || 0;
-	res.render('index', {count: count, bg: bg});
+	res.render(template, {count: count, bg: bg});
+}
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	home('index', req, res);
+});
+
+router.get('/banner', function(req, res) {
+	home('banner', req, res);
 });
 
 module.exports = router;
